558ba5937965b77ee729b52125e2c296fd7288ad
Nati T <natnaelteklu200@gmail.com>
2024-10-04 19:07:25 +0300

    ActionListener added

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 
-const daysTable = document.getElementsByClassName("daysTable");
+const daysTable = document.getElementsByTagName("table")[0];
 
-for(var i = 0; i <= daysTable.length; i++){
-    // console.log(daysTable.item(i).innerHTML)
+// console.log(daysTable.innerHTML)
 
-    const d = daysTable.item(i).getAttributeNames()
-    console.log(d)
-   if(d.innerHTML == "td"){
-    console.log("got a row")
-   }
-   else{
-    continue;
-   }
- 
+const rows= daysTable.rows
+
+
+const days = []
+
+console.log(rows.item(0).children) //Header 
+
+
+console.log("number of rows(including header) " + rows.length)
+let cellcount = 0
+
+for(var i = 1; i <= rows.length-1; i++){
+
+    for(var j = 0 ; j < 7 ; j++ ){
+    if(cellcount < 4 || cellcount >= 60+4){
+
+    }
+    else {
+        rows.item(i).children.item(j).firstElementChild.textContent = cellcount -3
+        rows.item(i).children.item(j).addEventListener("dblclick",function (e){
+            console.log(this.textContent);
+        })
+    }
+           
+        
+        
+        console.log(cellcount)
+        // console.log(rows.length)
+        cellcount += 1
+    }
+  
+    console.log("row" + i)
+  
 }
+// console.log(days)
 
 
 
